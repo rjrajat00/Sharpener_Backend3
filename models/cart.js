68d909762b4e7107ddaff5e72734f73dfbfde6fe
Rajat SIngh <rjrajat00@gmail.com>
2023-07-04 00:36:07 +0530

    Dynamic Routes part 2

---
+++ models/cart.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+const path = require("path");
+
+const p = path.join(path.dirname(require.main.filename), "data", "cart.json");
+
+module.exports = class Cart {
+  static addProduct(id, productPrice) {
+    fs.readFile(p, (err, fileContent) => {
+      let cart = { products: [], totalPrice: 0 }; // Initialize cart with empty products array
+
+      if (!err) {
+        cart = JSON.parse(fileContent);
+      }
+
+      const existingProductIndex = cart.products.findIndex(
+        (prod) => prod.id === id
+      );
+      const existingProduct = cart.products[existingProductIndex];
+      let updatedProduct;
+
+      if (existingProduct) {
+        updatedProduct = { ...existingProduct };
+        updatedProduct.qty += 1;
+        cart.products[existingProductIndex] = updatedProduct;
+      } else {
+        updatedProduct = { id: id, qty: 1 };
+        cart.products.push(updatedProduct);
+      }
+      cart.totalPrice = cart.totalPrice + +productPrice;
+
+      fs.writeFile(p, JSON.stringify(cart), (err) => {
+        if (err) {
+          console.log(err);
+        }
+      });
+    });
+  }
+};
